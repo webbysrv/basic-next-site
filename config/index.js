670d403bda837be6d91bdc236b91da5b82fd22f9
webbysrv <webbysrv@local>
2022-05-08 13:45:59 +0530

    empty server env var for remote

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://basic-next-site.vercel.app/";
+export const server = dev ? "http://localhost:3000" : "";
